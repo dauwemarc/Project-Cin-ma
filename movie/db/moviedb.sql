a30bc77575a7bd51517c93b9ed6ee17c6762504c
dauwemarc <56584311+dauwemarc@users.noreply.github.com>
2020-03-17 10:25:12 +0100

    Add files via upload

--- movie/db/moviedb.sql
+++ movie/db/moviedb.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 14, 2017 at 06:02 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 7.0.13
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mar. 17 mars 2020 à 09:24
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,54 +19,58 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `moviedb`
+-- Base de données :  `moviedb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `movielist`
+-- Structure de la table `movielist`
 --
 
-CREATE TABLE `movielist` (
-  `movieId` int(11) NOT NULL,
+DROP TABLE IF EXISTS `movielist`;
+CREATE TABLE IF NOT EXISTS `movielist` (
+  `movieId` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) DEFAULT NULL,
   `Genre` varchar(25) DEFAULT NULL,
   `Director` varchar(255) DEFAULT NULL,
   `Description` varchar(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
-  `imdb` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `imdb` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`movieId`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `movielist`
+-- Déchargement des données de la table `movielist`
 --
 
 INSERT INTO `movielist` (`movieId`, `Name`, `Genre`, `Director`, `Description`, `image`, `imdb`) VALUES
-(1, 'Batman Begins', 'Action', 'Christopher Nolan', 'A young Bruce Wayne (Christian Bale) travels to the Far East, where he''s trained in the martial arts by Henri Ducard (Liam Neeson), a member of the mysterious League of Shadows. ', 'batman.jpg', '9.5'),
-(2, 'Spider-Man: Homecoming (2017)', 'Adventure', 'Jon Watts', 'Thrilled by his experience with the Avengers, young Peter Parker returns home to live with his Aunt May. Under the watchful eye of mentor Tony Stark, Parker starts to embrace his newfound identity as Spider-Man.', 'spiderman.jpg', '8.5'),
-(3, 'Fight Club', 'Action', 'David Fincher', 'A depressed man (Edward Norton) suffering from insomnia meets a strange soap salesman named Tyler Durden (Brad Pitt) and soon finds himself living in his squalid house after his perfect apartment is destroyed. The two bored men form an underground club wi', 'fightClub.jpg', '8.8'),
-(4, 'Gladiator', 'Action', 'Ridley Scott', 'Commodus (Joaquin Phoenix) takes power and strips rank from Maximus (Russell Crowe), one of the favored generals of his predecessor and father, Emperor Marcus Aurelius, the great stoical philosopher. Maximus is then relegated to fighting to the death in t', 'gladiator.jpg', '8.5'),
-(5, 'Star Wars', 'Adventure', 'George Lucas', 'Star Wars is an American epic space opera franchise, centered on a film series created by George Lucas. It depicts the adventures of various characters "a long time ago in a galaxy far, far away"', 'starwars.png', '9'),
-(6, 'Despicable Me 3', 'Comedy', 'Pierre Coffin, Kyle Balda', 'The mischievous Minions hope that Gru will return to a life of crime after the new boss of the Anti-Villain League fires him. Instead, Gru decides to remain retired and travel to Freedonia to meet his long-lost twin brother for the first time. The reunite', 'despicableme.jpg', '6.4');
+(1, 'Batman Begins', 'Action', 'Christopher Nolan', 'Comment un homme seul peut-il changer le monde ? Telle est la question qui hante Bruce Wayne depuis cette nuit tragique où ses parents furent abattus sous ses yeux, dans une ruelle de Gotham City', 'batman.jpg', '9.5'),
+(2, 'Spider-Man: Homecoming (2017)', 'Adventure', 'Jon Watts', 'Après ses spectaculaires débuts avec les Avengers, le jeune Peter Parker découvre peu à peu sa nouvelle identité, celle de Spider-Man, le superhéros lanceur de toile.', 'spiderman.jpg', '8.5'),
+(3, 'Fight Club', 'Action', 'David Fincher', 'Jack est un jeune expert en assurance insomniaque, désillusionné par sa vie personnelle et professionnelle.', 'fightClub.jpg', '8.8'),
+(4, 'Gladiator', 'Action', 'Ridley Scott', 'Le général romain Maximus est le plus fidèle soutien de l\'empereur Marc Aurèle, qu\'il a conduit de victoire en victoire.', 'gladiator.jpg', '8.5'),
+(5, 'Star Wars', 'Adventure', 'George Lucas', 'Star Wars est un univers de fantasy et de science-fiction créé par George Lucas. D\'abord conçue comme une trilogie cinématographique sortie entre 1977 et 1983', 'starwars.png', '9'),
+(6, 'Despicable Me 3', 'Comedy', 'Pierre Coffin, Kyle Balda', 'Gru voyage à Freedonia pour rencontrer son frère jumeau pour la première fois. Les frères décident de former une alliance et de faire tomber Balthazar Bratt', 'despicableme.jpg', '6.4');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `showorder`
+-- Structure de la table `showorder`
 --
 
-CREATE TABLE `showorder` (
-  `showOrderId` int(11) NOT NULL,
+DROP TABLE IF EXISTS `showorder`;
+CREATE TABLE IF NOT EXISTS `showorder` (
+  `showOrderId` int(11) NOT NULL AUTO_INCREMENT,
   `date` varchar(255) NOT NULL,
   `timeslot` varchar(255) NOT NULL,
   `theater` varchar(255) NOT NULL,
   `movieName` varchar(255) NOT NULL,
-  `seat` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `seat` varchar(255) NOT NULL,
+  PRIMARY KEY (`showOrderId`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `showorder`
+-- Déchargement des données de la table `showorder`
 --
 
 INSERT INTO `showorder` (`showOrderId`, `date`, `timeslot`, `theater`, `movieName`, `seat`) VALUES
@@ -79,17 +85,19 @@ INSERT INTO `showorder` (`showOrderId`, `date`, `timeslot`, `theater`, `movieNam
 -- --------------------------------------------------------
 
 --
--- Table structure for table `theater`
+-- Structure de la table `theater`
 --
 
-CREATE TABLE `theater` (
-  `theaterId` int(11) NOT NULL,
+DROP TABLE IF EXISTS `theater`;
+CREATE TABLE IF NOT EXISTS `theater` (
+  `theaterId` int(11) NOT NULL AUTO_INCREMENT,
   `theaterName` varchar(255) DEFAULT NULL,
-  `seat` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `seat` int(11) NOT NULL,
+  PRIMARY KEY (`theaterId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `theater`
+-- Déchargement des données de la table `theater`
 --
 
 INSERT INTO `theater` (`theaterId`, `theaterName`, `seat`) VALUES
@@ -101,16 +109,18 @@ INSERT INTO `theater` (`theaterId`, `theaterName`, `seat`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `timeslot`
+-- Structure de la table `timeslot`
 --
 
-CREATE TABLE `timeslot` (
-  `timeslotId` int(11) NOT NULL,
-  `time` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `timeslot`;
+CREATE TABLE IF NOT EXISTS `timeslot` (
+  `timeslotId` int(11) NOT NULL AUTO_INCREMENT,
+  `time` varchar(255) NOT NULL,
+  PRIMARY KEY (`timeslotId`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `timeslot`
+-- Déchargement des données de la table `timeslot`
 --
 
 INSERT INTO `timeslot` (`timeslotId`, `time`) VALUES
@@ -123,87 +133,27 @@ INSERT INTO `timeslot` (`timeslotId`, `time`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Structure de la table `user`
 --
 
-CREATE TABLE `user` (
-  `userId` int(11) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
   `userName` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `status` int(3) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` int(3) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`userId`, `userName`, `password`, `status`) VALUES
 (1, 'admin', 'admin', 101),
 (3, 'user', 'user', 202);
+COMMIT;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `movielist`
---
-ALTER TABLE `movielist`
-  ADD PRIMARY KEY (`movieId`);
-
---
--- Indexes for table `showorder`
---
-ALTER TABLE `showorder`
-  ADD PRIMARY KEY (`showOrderId`);
-
---
--- Indexes for table `theater`
---
-ALTER TABLE `theater`
-  ADD PRIMARY KEY (`theaterId`);
-
---
--- Indexes for table `timeslot`
---
-ALTER TABLE `timeslot`
-  ADD PRIMARY KEY (`timeslotId`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `movielist`
---
-ALTER TABLE `movielist`
-  MODIFY `movieId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `showorder`
---
-ALTER TABLE `showorder`
-  MODIFY `showOrderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `theater`
---
-ALTER TABLE `theater`
-  MODIFY `theaterId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `timeslot`
---
-ALTER TABLE `timeslot`
-  MODIFY `timeslotId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
